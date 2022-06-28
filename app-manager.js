203cccb96d5cfcecc18a627ef6d71d5f528f6c02
Sagi Mines <sagi1236@gmail.com>
2022-06-28 17:23:52 +0300

    image fixed

--- app-manager.js
+++ app-manager.js
@@ -17,16 +17,3 @@ const getPhase3 = () => {
     }
   }
 };
-
-// const getBase64Image = bannerImage => {
-//   let canvas = document.createElement('canvas');
-//   canvas.width = img.width;
-//   canvas.height = img.height;
-
-//   let ctx = canvas.getContext('2d');
-//   ctx.drawImage(img, 0, 0);
-
-//   let dataURL = canvas.toDataURL('image/png');
-
-//   return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
-// };
