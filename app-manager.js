47da45d65316e507f365cbb5c52ca28b7e5ca433
oferovadia <ovadiaofer@gmail.com>
2022-06-28 18:21:16 +0300

    function editing

--- app-manager.js
+++ app-manager.js
@@ -1,22 +1,22 @@
 
 const setPhase3 = (imgUrl, hobbiesArr) => {
-  localStorage.setItem('image', JSON.stringify(imgUrl));
-  localStorage.setItem('checkedHobbies', JSON.stringify(hobbiesArr));
-  localStorage.setItem('phase3', JSON.stringify('v'));
+    localStorage.setItem('image', JSON.stringify(imgUrl));
+    localStorage.setItem('checkedHobbies', JSON.stringify(hobbiesArr));
+    localStorage.setItem('phase3', JSON.stringify('v'));
 };
 
 const getPhase3 = () => {
-  if (localStorage.getItem('phase3')) {
-    const hobbies = localStorage.getItem('checkedHobbies');
-    const fileUpload = document.querySelector('#fileUpload').nextElementSibling;
-    fileUpload.src = JSON.parse(localStorage.getItem('image'));
-    const hobbieInputs = document.querySelectorAll('input[type=checkbox]');
-    for (const hobbieInput of hobbieInputs) {
-      if (hobbies.includes(hobbieInput.nextSibling.innerHTML)) {
-        hobbieInput.checked = true;
-      }
+    if (localStorage.getItem('phase3')) {
+        const hobbies = localStorage.getItem('checkedHobbies');
+        const fileUpload = document.querySelector('#fileUpload').nextElementSibling;
+        fileUpload.src = JSON.parse(localStorage.getItem('image'));
+        const hobbieInputs = document.querySelectorAll('input[type=checkbox]');
+        for (const hobbieInput of hobbieInputs) {
+            if (hobbies.includes(hobbieInput.nextSibling.innerHTML)) {
+                hobbieInput.checked = true;
+            }
+        }
     }
-  }
 };
 
 function saveEntranceData() {
@@ -41,18 +41,16 @@ function getPhase2LocalStorage() {
 }
 
 function canYouMoveToThisPage() {
-    if (!localStorage.getItem("agree")) {
+    if (!localStorage.getItem("phase1Data") && window.location.href == document.referrer) {
         window.location.replace(`${document.location.origin}/the-wizard/entrance-screen/entrance.html`)
-    } else if (!localStorage.getItem("phase1Data")) {
+    } else if (!localStorage.getItem("phase-2") && window.location.href == document.referrer) {
         window.location.replace(`${document.location.origin}/the-wizard/phase-1/phase1.html`)
-    } else if (!localStorage.getItem("phase-2")) {
+    } else if (!localStorage.getItem("img") && window.location.href == document.referrer) {
         window.location.replace(`${document.location.origin}/the-wizard/phase-2/phase-2.html`)
-    } else if (!localStorage.getItem("img")) {
-        window.location.replace(`${document.location.origin}/the-wizard/phase-3/phase-3.html`)
     }
 }
 
-function startNewWizard(){
+function startNewWizard() {
     localStorage.clear()
 }
 
